43452e27bc79c83083f5b0805918ece93af19208
Daniel Villalobos del Baño <daniel.villalobosdel@gmail.com>
2020-05-15 17:25:30 +0200

    Center by default

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,14 +1,14 @@
 module.exports = {
   purge: {
-    enabled: process.env.NODE_ENV === 'production',
-    content: [
-      './public/**/*.html',
-      './src/**/*.vue'
-    ]
+    enabled: process.env.NODE_ENV === "production",
+    content: ["./public/**/*.html", "./src/**/*.vue"]
   },
   theme: {
-    extend: {},
+    container: {
+      center: true
+    },
+    extend: {}
   },
   variants: {},
-  plugins: [],
-}
+  plugins: []
+};
