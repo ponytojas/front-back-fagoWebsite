f73a1f8a53b05842c48fcb1d20f1d8f2c70c14de
Daniel Villalobos del Baño <daniel.villalobosdel@gmail.com>
2020-05-15 17:29:06 +0200

    Add axios for the Auth header

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,9 +2,11 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import '@/assets/style/tailwind.css'
+import Axios from "axios";
+import "@/assets/style/tailwind.css";
 
 Vue.config.productionTip = false;
+Axios.defaults.headers.common["Authorization"] = `Bearer ${store.state.token}`;
 
 new Vue({
   router,
