82aac591de40df3223edc79d34741889da1a9c3c
Daniel Villalobos del Baño <daniel.villalobosdel@gmail.com>
2020-05-15 17:26:51 +0200

    For now we have a button to go for the login page

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="container">
+      <router-link class="m-5 p-5" to="/login">
+        <button
+          class="m-5 px-10 py-2 bg-blue-500 hover:bg-blue-700 text-white rounded-full"
+        >
+          Login
+        </button>
+      </router-link>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
-  components: {
-    HelloWorld
-  }
+  components: {}
 };
 </script>
