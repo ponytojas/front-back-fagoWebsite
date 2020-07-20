d5647f6148faee9b372e733c226c88fd03ee06ed
Daniel Villalobos <daniel.villalobosdel@gmail.com>
2020-07-20 19:59:35 +0200

    Show articles

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -9,6 +9,11 @@
           aria-label="Busca un artículo"
         />
       </div>
+      <div class="grid grid-cols-3 gap-4 container mx-auto">
+        <div v-for="article in articles" v-bind:key="article.article_id">
+          <ArticleCard :article="article" class="pb-4 mb-3" />
+        </div>
+      </div>
       <router-link class="m-5 p-5" to="/login">
         <button
           class="m-5 px-10 py-2 bg-blue-500 hover:bg-blue-700 text-white rounded-full"
@@ -23,11 +28,13 @@
 <script>
 import Autocomplete from "@trevoreyre/autocomplete-vue";
 import axios from "axios";
+import ArticleCard from "../components/articlesCard";
 
 export default {
   name: "Home",
   components: {
     Autocomplete,
+    ArticleCard,
   },
   data() {
     return {
@@ -37,8 +44,11 @@ export default {
   },
   async mounted() {
     await axios
-      .get("http://localhost:3000/articles/")
-      .then((response) => (this.articles = response.data));
+      .get("http://localhost:3000/get-data-for-web/")
+      .then((response) => {
+        console.log(response.data);
+        this.articles = response.data;
+      });
     await this.$store
       .dispatch("articles", this.articles)
       .then(() => (this.loaded = true));
