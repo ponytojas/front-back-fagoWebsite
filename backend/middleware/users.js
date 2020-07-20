2e2c28b5646b9ec3c768292d8f5b9513cd72a2c1
Daniel Villalobos <daniel.villalobosdel@gmail.com>
2020-07-20 12:09:13 +0200

    Better way to log the middleware

--- backend/middleware/users.js
+++ backend/middleware/users.js
@@ -4,40 +4,53 @@ const db = require('../lib/db.js');
 
 module.exports = {
     validateRegister: (req, res, next) => {
-        // username min length 3
+        console.log("Checking if username is correct")
+        console.log("Username => " + req.body.username)
         if (!req.body.username || req.body.username.length < 3) {
+            console.log("The username is not at least 3 chars")
             return res.status(400).send({
                 msg: 'Please enter a username with min. 3 chars'
             });
         }
-
-        // password min 6 chars
+        console.log("Username correct")
+        console.log("Checking if password is correct")
+        console.log("Password => " + req.body.password)
         if (!req.body.password || req.body.password.length < 6) {
+            console.log("The password is not at least 6 chars")
             return res.status(400).send({
                 msg: 'Please enter a password with min. 6 chars'
             });
         }
-        // password (repeat) does not match
+        console.log("Password correct")
+        console.log("Checking if password_repeat is correct")
+        console.log("Password_repeat => " + req.body.password_repeat)
         if (
             !req.body.password_repeat ||
             req.body.password !== req.body.password_repeat
         ) {
+            console.log("Password repeat is not the same")
             return res.status(400).send({
                 msg: 'Both passwords must match'
             });
         }
+        console.log("Password correct")
+        console.log("Everything correct, creating user")
         next();
     },
 
     isLoggedIn: (req, res, next) => {
+        console.log("Checking if user is loggedIn")
+        console.log("JWT => " + req.headers.authorization)
         try {
             const token = req.headers.authorization;
             req.userData = jwt.verify(
                 token,
                 'SuperSecretKeyUsed'
             );
+            console.log("Correct user session")
             next();
         } catch (err) {
+            console.log("There was an error checking the session")
             return res.status(401).send({
                 msg: 'Your session is not valid!'
             });
@@ -45,6 +58,8 @@ module.exports = {
     },
 
     isSuperUser: (req, res, next) => {
+        console.log("Checking if user is SuperUser")
+        console.log("JWT => " + req.headers.authorization)
         try {
             const token = req.headers.authorization;
             req.userData = jwt.verify(
@@ -56,14 +71,19 @@ module.exports = {
             db.query(text, values, (err, result) => {
                 if (err)
                     return res.status(400).send({msg: err})
-                else if (result.rows[0].is_superuser)
+                else if (result.rows[0].is_superuser) {
+                    console.log("Correct superuser session")
                     next()
-                else
+                }
+                else {
+                    console.log("Incorrect superuser session")
                     return res.status(401).send({
                         msg: 'Your session is not valid!'
                     });
+                }
             });
         } catch (err) {
+            console.log("There was an error checking the session")
             return res.status(401).send({
                 msg: 'Your session is not valid!'
             });
