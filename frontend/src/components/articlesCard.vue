9b98c22d7330bf4b6391d13b4725d97ab9b6a6c0
Daniel Villalobos <daniel.villalobosdel@gmail.com>
2020-07-20 19:59:44 +0200

    Show articles

---
+++ frontend/src/components/articlesCard.vue
@@ -0,0 +1,33 @@
+<template>
+  <a href="#">
+    <div class="border rounded-md pb-3 mb-5">
+      <p class="text-xl pt-3 pb-3">{{ article.title }}</p>
+      <p class="text-xl pt-3 pb-3">{{ article.subtitle }}</p>
+      <span v-for="(tag, index) in tags" :key="index" class="text-xs pt-3 pb-5"
+        >· {{ tag }} //
+      </span>
+    </div>
+  </a>
+</template>
+
+<script>
+export default {
+  props: {
+    article: {
+      type: Object,
+      required: true,
+    },
+  },
+  components: {},
+  data() {
+    return {
+      tags: [],
+    };
+  },
+  methods: {},
+  beforeMount() {
+    console.log(this.article.tags);
+    this.tags = this.article.tags;
+  },
+};
+</script>
