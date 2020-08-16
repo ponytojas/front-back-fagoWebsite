3b9e1e2a4e1a2523fca9d576c50a3d243d8756b9
Daniel Villalobos <daniel.villalobosdel@gmail.com>
2020-08-16 19:33:57 +0200

    Getting image handler and upload working (Not showing image on localhost) Force width=500 for now on images multer added to handle images on backend

--- frontend/src/views/Editor.vue
+++ frontend/src/views/Editor.vue
@@ -2,7 +2,12 @@
   <div class="w-screen h-screen bg-yellow-400 absolute overflow-auto">
     <div class="flex justify-center align-middle h-full bg-yellow-400 relative">
       <div class="mt-12 h-full w-9/12 mb-12">
-        <vue-editor v-model="content" class="bg-white" />
+        <vue-editor
+          v-model="content"
+          class="bg-white"
+          useCustomImageHandler
+          @imageAdded="handleImageAdded"
+        />
       </div>
     </div>
   </div>
@@ -10,7 +15,7 @@
 
 <script>
 import { VueEditor } from "vue2-editor";
-import Axios from "axios";
+import axios from "axios";
 
 export default {
   components: {
@@ -19,31 +24,45 @@ export default {
   data() {
     return {
       content: "<h1>Some initial content</h1>",
+      file: "",
     };
   },
   methods: {
-    handleImageAdded: function (file, Editor, cursorLocation, resetUploader) {
-      // An example of using FormData
-      // NOTE: Your key could be different such as:
-      // formData.append('file', file)
+    handleImageAdded: function (file, Editor, cursorLocation) {
+      console.log("Inserting image to server");
+      this.file = file;
 
-      var formData = new FormData();
-      formData.append("image", file);
+      let formData = new FormData();
+      formData.append("editor-image", this.file); // appending file
 
-      Axios({
-        url: "https://fakeapi.yoursite.com/images",
-        method: "POST",
-        data: formData,
-      })
-        .then((result) => {
-          let url = result.data.url; // Get url from response
-          Editor.insertEmbed(cursorLocation, "image", url);
-          resetUploader();
+      // sending file to the backend
+      axios
+        .post("http://localhost:3000/editor-images", formData)
+        .then((res) => {
+          console.log(res.data);
+          let url = res.data;
+          Editor.insertEmbed(
+            cursorLocation,
+            "image",
+            "http://localhost:3000" + url
+          );
+          this.insertImageWidth(url);
         })
         .catch((err) => {
           console.log(err);
         });
     },
+    insertImageWidth(fileUploaded) {
+      console.log("Hola");
+      console.log(this.content);
+      this.content = this.content.replace(
+        fileUploaded + '"',
+        fileUploaded + '" width="500"'
+      );
+      console.log(this.content);
+    },
   },
 };
 </script>
+
+//Editor, cursorLocation, resetUploader
