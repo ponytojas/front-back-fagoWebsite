ba98bdcd97fc84921458e7493021c75ad67acf78
Daniel Villalobos <daniel.villalobosdel@gmail.com>
2020-10-03 20:40:35 +0200

    New helmet package

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,13 @@ const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const logger = require("./lib/logger")
+const helmet = require("helmet")
 
 // set up port
 const PORT = process.env.PORT || 3000;
 app.use(bodyParser.json());
 app.use(cors());
+app.use(helmet())
 
 // add routes
 const router = require("./routes/router.js");
